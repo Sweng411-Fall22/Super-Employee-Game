8c39e38ace5d456036f007afc09f53ecf3d409e3
reidrm <113366533+reidrm@users.noreply.github.com>
2022-11-06 19:05:02 -0500

    Add files via upload
    This has all of the files split up solely by whenever there was a new class.  I started to fix all the issues that caused (i.e. importing things to each file, adding 'filename.' before variables from other modules, etc) however I felt like there is going to be further restructuring beyond this, so I would like to talk to everyone about what is going where (and definitely creating a settings module and a main class for the game itself) before doing a bunch of work that would just get redone in the coming weeks.

---
+++ water.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class Water(pygame.sprite.Sprite):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+	def update(self):
+		self.rect.x += screen_scroll
